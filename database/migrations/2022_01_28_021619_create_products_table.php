39943c196b5c0b9c5ac7a691eafa94dc506e95d3
Pedro Gabriel <gaboleal123@gmail.com>
2022-01-27 22:42:31 -0400

    migraciones tabla productos y marcas

---
+++ database/migrations/2022_01_28_021619_create_products_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name',255);
+            $table->char('size',1);
+            $table->string('observations',255);
+            $table->foreignId('id_brand');
+            $table->integer('quantity_inventory');
+            $table->date('boarding_date');
+            $table->timestamps();
+
+            $table->foreign('id_brand')->references('id')->on('brands')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
